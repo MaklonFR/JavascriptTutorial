fa6d2fa6678151101974fd0cc8a054caeede1ef3
MaklonFR <kallonjuve@gmail.com>
2025-02-18 21:19:44 +0700

    update

---
+++ praktikum-2/script.js
@@ -0,0 +1,23 @@
+//Setiap perintah diakhiri dengan tanda titik koma ;
+console.log("Halo, dunia!");
+
+/*
+JavaScript bersifat case-sensitive
+nama dan Nama adalah dua variabel yang berbeda. 
+*/
+let nama = 'MaklonFr'; 
+let Nama = 'Malon Frare'
+
+console.log(nama, Nama);
+
+/*
+JavaScript mengabaikan spasi kosong dan indentasi
+variabel namaLengkap tidak menggunakan spasi
+*/ 
+let namaLengkap = 'Maklon J. Frare';
+console.log(namaLengkap);
+
+//Menggunakan komentar ( Komentar satu baris)
+/*
+Ini adalah komentar multi-baris
+*/
---
+++ praktikum-3/script.js
@@ -0,0 +1,11 @@
+//1. Menggunakan var (cara lama, kurang direkomendasikan) 
+var nama = "Budi";
+console.log(nama);
+
+//2. Menggunakan let (disarankan) 
+let umur = 25;
+console.log(umur);
+
+//3. Menggunakan const (untuk nilai tetap) 
+const PI = 3.14;
+console.log(PI);
